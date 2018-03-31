a4f166977917dd439eed9a26e1e1282f8032f16b
avagen <37959809+avagen@users.noreply.github.com>
2018-03-31 19:07:44 +0300

    Create avagen.js

---
+++ avagen.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name            Avagen
+// @version         0.1
+// @namespace       Uyutniy thread
+// @author          Anonymouse
+// @description     Nu tipa generiruet avatarku i tebya banyat ili skrivayut v etom itt in this thread trede.
+// @run-at          document-end
+// @grant           unsafeWindow
+// @include         http://dobrochan.ru/b/res/*
+// @nocompat        Chrome
+// ==/UserScript==
